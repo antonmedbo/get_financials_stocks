572c669352ebb70f8e92c4e31fcdfb3318f54727
Anton Medbo <medbo95@gmail.com>
2023-07-31 15:03:22 +0200

    fix

--- main.py
+++ main.py
@@ -3,12 +3,20 @@ import psycopg2
 from utils.check import get_date, three_months, which_metric, get_multiplier
 from pprint import pprint
 from utils.check_document_name import year_from_name
-from scraper.ABB_scraper import get_excel_files_ABB
+from scraper.ABB_scraper import get_excel_files
 import json
 from datetime import datetime
 from insert_into_postgres.insert import add_financial_statement
 
-list_income_statements = get_excel_files_ABB()
+# Load company configuration
+with open("config/config.json") as config_file:
+    configs = json.load(config_file)
+
+# Get data for each company
+for company, config in configs.items():
+    print(f"Getting data for {company}...")
+    list_income_statements = get_excel_files(config)
+
 print(list_income_statements)
 def datetime_converter(o):
     if isinstance(o, datetime):
--- scraper/ABB_scraper.py
+++ scraper/ABB_scraper.py
@@ -1,43 +1,35 @@
 import io
+import json
 import requests
 from bs4 import BeautifulSoup
 from openpyxl import load_workbook
 from zipfile import BadZipFile
 from extracter.get_data import from_excel
 
-def get_excel_files_ABB():
+def get_excel_files(company_config):
     list_income_statements = []
-    response = requests.get("https://global.abb/group/en/investors/quarterly-results")
+    response = requests.get(company_config["results_url"])
     soup = BeautifulSoup(response.content, 'html.parser')
 
-    # Fetch all the links that contain 'XLSX' in their text
-    links = soup.find_all('a', text=lambda text: text and "xlsx" in text.lower())
+    # Use the link pattern from the configuration
+    links = soup.find_all('a', text=lambda text: text and company_config["link_pattern"] in text.lower())
 
     for link in links:
         url = link['href']
 
-        # Some links might be relative, so we need to make them absolute
         if url.startswith("/"):
-            url = "https://global.abb.com" + url
+            url = company_config["base_url"] + url
 
-        # Get the parent tr of the a element
         parent_tr = link.find_parent('tr')
-
-        # Get the text of the first td in the parent tr, and extract the year
         year = parent_tr.find('td').get_text(strip=True).split(' ')[1]
 
         print(url)
 
-        # Get the content of the excel file
         response = requests.get(url)
         content = response.content
 
-
         try:
-            # Load the workbook into memory
             workbook = load_workbook(filename=io.BytesIO(content))
-
-            # For example, print out the values of the first sheet
             sheet = workbook.active
 
             income_statement = from_excel(sheet, year)
